453ff7b80804e4cfc64f94f2b1172ef2aacfd6f0
Daniel Merkl <dan.merkl.90@gmail.com>
2019-09-24 09:49:03 -0500

    just adding html

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,9 +1,28 @@
-const path = require("path")
+
+const path = require("path");
+const express = require("express");
+const apiRouter = express.Router();
+
+const friends = require("../data/friends.js");
 
 app.get("/api/friends", function(req, res) {
+    res.json(friends);
     
-})
+});
 
 app.post("/api/friends", function(req,res) {
     
-})
+    friends.create([
+        "name", "sleepy"
+      ], [
+        req.body.name, req.body.sleepy
+      ], function(result) {
+        // Send back the ID of the new quote
+        res.json({ id: result.insertId });
+      });
+    });
+
+
+
+
+module.exports = apiRouter;
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,3 +1,7 @@
+const express = require("express");
+const path = require("path");
+const htmlRouter = express.Router();
+
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "app/public/home.html"));
 });
@@ -6,3 +10,5 @@ app.get("/", function (req, res) {
 app.get("/survey", function (req, res) {
     res.sendFile(path.join(__dirname + "app/public/survey.html"));
 });
+
+module.exports = htmlRouter;
